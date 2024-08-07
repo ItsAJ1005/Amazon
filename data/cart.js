0620cb2e6bff7443697a054db9c88e332e6ab931
ItsAJ1005 <ajharshvardhan123@gmail.com>
2024-08-07 20:52:51 +0530

    add - delete func in cart

--- data/cart.js
+++ data/cart.js
@@ -1,8 +1,8 @@
-export const cart = [{
-  id: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+export let cart = [{
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 1
 },{
-  id: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity: 3
 }];
 
@@ -26,3 +26,17 @@ export function addToCart(productId){
     }
   }
   
+
+  export function removeFromCart(productId) {
+    const newCart = [];
+  
+    cart.forEach((cartItem) => {
+      if (cartItem.productId !== productId) {
+        newCart.push(cartItem);
+      }
+    });
+  
+    cart = newCart;
+  
+    // saveToStorage();
+  }
